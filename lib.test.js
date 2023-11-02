41d6cd778d65fe55906d35479cc98ec1c1a2ac7f
Jason <jason808@naver.com>
2023-11-02 11:08:33 +0900

    Implementing fact, prime

--- lib.test.js
+++ lib.test.js
@@ -1,10 +1,18 @@
 const { test, expect } = require("@jest/globals");
 const lib = require("./lib");
 
-test("(avg[3,5,7]) should be 5", () => {
+test("avg([3,5,7]) should be 5", () => {
     expect(lib.avg([3, 5, 7])).toBe(5);
 });
 
-test("avg([-5,5 ]) should be 0", () => {
+test("avg([-5,5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
+});
+
+test("prime([3]) is prime", () => {
+    expect(lib.prime([3])).toBe(true);
+});
+
+test("fact([4]) is 24", () => {
+    expect(lib.fact([4])).toBe(24);
 });
